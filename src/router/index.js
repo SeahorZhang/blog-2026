bb5816945fb5409de09fbdcded98de5d015d02dd
xihe <im0k@outlook.com>
2025-12-29 00:28:50 +0800

    Switch router history mode from Web History to Web Hash History for improved compatibility with hash-based routing.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomeView from '@/pages/home/index.vue'
 import NavVidw from '@/pages/widgets/navCard/index.vue'
@@ -40,6 +40,6 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
