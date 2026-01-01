93b980cf63e3f6c3da2445165cf0b2c3beb402b5
xihe <im0k@outlook.com>
2026-01-02 01:40:32 +0800

    feat: 优化社交链接显示逻辑，逐个显示并更新模板渲染

--- src/pages/home/components/socialButtons/index.vue
+++ src/pages/home/components/socialButtons/index.vue
@@ -23,21 +23,40 @@ const x = computed(() => props.center.x + hiCardWidth / 2 - width)
 const y = computed(() => props.center.y + hiCardHeight / 2 + props.cardSpacing)
 
 const show = ref(false)
+const visibleItems = ref(Array.from({ length: socialLinks.length }, () => false))
+
 watchEffect(() => {
   const delay = order * 0.2
   setTimeout(() => {
     show.value = true
+    // 逐个显示每个社交链接项
+    socialLinks.forEach((_, i) => {
+      setTimeout(() => {
+        visibleItems.value[i] = true
+      }, (socialLinks.length - i) * 100) // 每个项延迟 0.1 秒
+    })
   }, delay * 1000)
+})
+
+const visibleSocialLinks = computed(() => {
+  return socialLinks.filter((_, i) => visibleItems.value[i])
 })
 </script>
 
 <template>
-  <div v-if="show" class="absolute flex gap-2" :style="{ left: `${x}px`, top: `${y}px`, width: `${width}px` }">
+  <div
+    v-if="show"
+    class="absolute flex gap-2 justify-end"
+    :style="{ left: `${x}px`, top: `${y}px`, width: `${width}px` }"
+  >
     <motion.a
-      v-for="(item, i) in socialLinks" :key="i" :initial="{ opacity: 0, scale: 0.6 }"
-      :animate="{ opacity: 1, scale: 1 }" :while-hover="{ scale: 1.05 }" :transition="{
-        delay: (socialLinks.length - i) * 0.1,
-      }" href="https://github.com/SeahorZhang" target="_blank"
+      v-for="(item) in visibleSocialLinks"
+      :key="item.name"
+      :initial="{ opacity: 0, scale: 0.6 }"
+      :animate="{ opacity: 1, scale: 1 }"
+      :while-hover="{ scale: 1.05 }"
+      href="https://github.com/SeahorZhang"
+      target="_blank"
       class="flex items-center  rounded-xl border-2 border-white   text-white px-3 py-1.5 cursor-pointer"
       :style="{ backgroundColor: item.bgColor }"
     >
