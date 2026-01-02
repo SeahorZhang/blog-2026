7ce717a38ecdf05bb115bdc421b3f3ac31e228e6
xihe <im0k@outlook.com>
2026-01-02 19:01:09 +0800

    fix: 添加 Microsoft Clarity 依赖并初始化分析工具

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { Icon } from '@iconify/vue'
+import Clarity from '@microsoft/clarity'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router/index.js'
 import './styles/index.css'
 
+Clarity.init ('uv4bq7qzcr')
 const app = createApp(App)
 app.component('Icon', Icon)
 app.use(router).mount('#app')
