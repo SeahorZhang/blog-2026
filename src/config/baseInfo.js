be8bfc1c37bde1371dd20ae0f79584e4fb8f234f
xihe <im0k@outlook.com>
2026-01-03 20:19:59 +0800

    fix: 修复 github 跳转错误问题

--- src/config/baseInfo.js
+++ src/config/baseInfo.js
@@ -9,7 +9,7 @@ export const settings = {
 export const socialLinks = [
   {
     name: 'GitHub',
-    url: 'https://github.com/SeahorZhang/widgets_desktop',
+    url: 'https://github.com/SeahorZhang',
     icon: 'tabler-brand-github',
     bgColor: '#181717',
   },
@@ -31,7 +31,8 @@ export const projects = [
   {
     name: 'WTab',
     info: '浏览器首页导航',
-    description: '一款高度可定制的浏览器新标签页扩展，支持自定义布局、网站收藏和搜索引擎，为您提供更高效的网络导航体验。',
+    description:
+      '一款高度可定制的浏览器新标签页扩展，支持自定义布局、网站收藏和搜索引擎，为您提供更高效的网络导航体验。',
     url: 'https://chromewebstore.google.com/detail/wtab/diiielhbpbgalhohmfelhgaakdjcmkjl?authuser=0&hl=zh-CN',
     img: wtabImg,
     type: 'Chrome扩展',
@@ -39,7 +40,8 @@ export const projects = [
   {
     name: 'pinece-webext',
     info: '浏览器书签导出',
-    description: '便捷的书签管理工具，可一键导出浏览器书签为多种格式，支持云同步和跨浏览器迁移，帮助您整理和备份重要网站收藏。',
+    description:
+      '便捷的书签管理工具，可一键导出浏览器书签为多种格式，支持云同步和跨浏览器迁移，帮助您整理和备份重要网站收藏。',
     url: 'https://chromewebstore.google.com/detail/pinece-webext/oloddfdfgpipbngfcohnfpgdallhnoep?authuser=0&hl=zh-CN',
     img: pineceWebextImg,
     type: 'Chrome扩展',
@@ -47,7 +49,8 @@ export const projects = [
   {
     name: 'Sync Cookies',
     info: '多域名cookies同步',
-    description: '解决多域名网站登录问题的实用工具，自动同步不同域名之间的Cookie信息，提升用户在相关站点间的无缝体验。',
+    description:
+      '解决多域名网站登录问题的实用工具，自动同步不同域名之间的Cookie信息，提升用户在相关站点间的无缝体验。',
     url: 'https://chromewebstore.google.com/detail/sync-cookies/hcehoofohgpjngheemopifodkamccbij?authuser=0&hl=zh-CN',
     img: syncCookiesImg,
     type: 'Chrome扩展',
